772fbee0481eef63b3d080cb7755f5046e7cab39
igwgw <hanseul0204@sookmyung.ac.kr>
2025-09-25 16:07:25 +0900

    Update 02main.c

--- 02main.c
+++ 02main.c
@@ -3,17 +3,22 @@
 
 int main(int argc, char *argv[])
 {
-    int year;
+    unsigned int x;
+    int b;
     
-    printf("Input year : ");
-    scanf("%i" , &year);
-  
-    printf("Is the year %i leap year? %i\n", 
-    year,
-    (year%4==0)&&(year%100!=0)||(year%400==0)
-    );
-  
-  
+    printf("input a number : ");
+    scanf("%ui",&x);
+    
+    for (b=0; x != 0; x >>=1)
+    {
+        if(x&1)
+        {
+               b++;
+               }
+     }
+    
+    printf("The result is : %i\n", b);
+    
 
     system("PAUSE");	
     return 0;
