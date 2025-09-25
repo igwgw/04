50f8fa110a75c17125da9fa53c482ba7654ef075
igwgw <hanseul0204@sookmyung.ac.kr>
2025-09-25 15:41:58 +0900

    Create 02main.c

---
+++ 02main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int x, y;
+    
+    printf("Input two integers : ");
+    scanf("%i %i", &x, &y);
+  
+    printf("+ result is : %i\n", x+y);
+  
+    printf("- result is : %i\n", x-y);
+  
+    printf("* result is : &i\n", x*y);
+  
+    printf("/ result is : %i\n", x/y);
+  
+    printf("%% result is : %i\n", x%y);
+  
+
+    system("PAUSE");	
+    return 0;
+}
